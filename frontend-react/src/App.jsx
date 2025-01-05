1a4229c001ed74e0c04ae0fbca85cbec1c123ca1
AneekS <aneeksaha2020@gmail.com>
2025-01-06 00:11:50 +0530

    Changes in react code

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,23 +1,26 @@
-import { CreateTodo } from "./components/createTodo";
-import { Todos  } from "./components/Todos";
-import {useState} from "react";
-// specific way of state defintion 
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CreateTodo } from './components/createTodo'
+import { Todos } from './components/Todos'
+
+// useEffect hook
 function App() {
+  const [todos, setTodos] = useState([]);
 
-const[todos,setTodos]=useState([])
-
+  // fetch("http://localhost:3000/todos")
+  //   .then(async function(res) {
+  //     const json = await res.json();
+  //     setTodos(json.todos);
+  //   })
 
   return (
     <div>
-      <CreateTodo> </CreateTodo>
-      <Todos todos={[{title:"Task 1",
-      description:"Perform task 1",
-      completed: false
-      }]}></Todos>
+      <CreateTodo></CreateTodo>
+      <Todos todos={todos}></Todos>
     </div>
   )
 }
 
-
-
-export default App 
+export default App
